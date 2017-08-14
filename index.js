3cc17eb7b22911c3e0cb35b80487d02b299cc628
vidurar <vidura.rajapaksa@gmail.com>
2017-08-15 00:18:48 +0530

    changed file name

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
-require('./routes/authroutes')(app)
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 
