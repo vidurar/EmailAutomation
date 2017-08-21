815f50f0614c04df15625b7010b81e0e4054b9d3
vidurar <vidura.rajapaksa@gmail.com>
2017-08-21 20:31:37 +0530

    build config done

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ app.use(passport.session())
 require('./routes/authroutes')(app)
 require('./routes/billingroutes')(app)
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'))
+
+	const path = require('path')
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log('server running on port 5000'))
